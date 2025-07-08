d125705eee415b30123c37b5effebde10e5c9484
andrewprzybilla <andrewprzy@pm.me>
2025-07-08 12:15:22 -0700

    Refactor charts for isolation of services

---
+++ deployment/scripts/add-helm-repos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "Adding Kong Helm repo..."
+helm repo add kong https://charts.konghq.com
+
+echo "Adding Bitnami Helm repo..."
+helm repo add bitnami https://charts.bitnami.com/bitnami || true
+
+echo "Adding Airflow Helm repo..."
+helm repo add airflow-stable https://airflow-helm.github.io/charts
+
+echo "Adding Stable Helm repo..."
+helm repo add stable https://charts.helm.sh/stable
+
+echo "Adding MinIO Operator Helm repo..."
+helm repo add minio-operator https://operator.min.io
+
+echo "Updating all Helm repos..."
+helm repo update
+
+echo "All official Helm repositories have been added and updated!"
